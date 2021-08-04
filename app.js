b53ca8fa4f7e40bef7b14fb5a9802f24d8646fe2
kenxpro <44691869+kenxpro@users.noreply.github.com>
2021-08-05 01:32:46 +0700

    Add Evironment Vars

---
+++ app.js
@@ -0,0 +1,77 @@
+//jshint esversion:6
+require('dotenv').config();
+const
+  express = require('express'),
+  ejs     = require('ejs'),
+  bodyParser = require('body-parser'),
+  mongoose   = require('mongoose'),
+  encrypt    = require('mongoose-encryption')
+  app        = express();
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+// console.log();
+
+mongoose.connect('mongodb://localhost:27017/userDB', {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+
+
+userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields: ['password']});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/',(req,res)=>{
+  res.render('home');
+});
+
+app.get('/login',(req,res)=>{
+  res.render('login')
+})
+
+app.get('/register',(req,res)=>{
+  res.render('register')
+})
+
+app.post('/register', (req,res)=>{
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+});
+
+newUser.save(err=>{
+  if (err) {
+    console.log(err);
+  } else {
+    res.render('secrets');
+  }
+})
+
+})
+
+app.post('/login',(req,res)=>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: username}, (err, foundUser)=>{
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render('secrets')
+        }
+      }
+    }
+  })
+})
+
+app.listen(3000, (req,res)=>{
+  console.log('Connected');
+});
